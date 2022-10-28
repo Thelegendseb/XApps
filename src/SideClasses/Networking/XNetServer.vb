fff4c0d0b79601d96d558c11032e3789a5dc87fb
Sebastian Clarke <SebClarke1@outlook.com>
2022-10-28 15:55:11 +0100

    fixed server

--- src/SideClasses/Networking/XNetServer.vb
+++ src/SideClasses/Networking/XNetServer.vb
@@ -3,9 +3,11 @@ Namespace Networking
     Public Class XNetServer
         Inherits XNetNode
         Public Event ClientReceived(AcceptedSocket As Socket)
-        Protected Overrides Sub Initialize()
-            MyBase.Initialize()
-            Me.BindSocket("127.0.0.1", 8888)
+        Sub New()
+            MyBase.New()
+        End Sub
+        Sub New(SocketIn As Socket)
+            MyBase.New(SocketIn)
         End Sub
         Protected Overrides Sub OnListen()
             Me.Socket_.Listen(0)
